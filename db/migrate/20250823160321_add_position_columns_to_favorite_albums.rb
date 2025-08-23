937d03485a0d5d16c23c7ed31fb47314287c668b
syouti <syoutiroukun@gmail.com>
2025-08-24 01:03:26 +0900

    Add position_x and position_y columns to favorite_albums

---
+++ db/migrate/20250823160321_add_position_columns_to_favorite_albums.rb
@@ -0,0 +1,6 @@
+class AddPositionColumnsToFavoriteAlbums < ActiveRecord::Migration[7.2]
+  def change
+    add_column :favorite_albums, :position_x, :integer
+    add_column :favorite_albums, :position_y, :integer
+  end
+end
