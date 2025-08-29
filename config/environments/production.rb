a7b21ae354c1727a82cbd9943e4ab42fa789dc78
syouti <syoutiroukun@gmail.com>
2025-08-30 06:41:01 +0900

    ActionMailer設定を修正: default_fromをApplicationMailerで設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_from = ENV['MAIL_FROM'] || 'TuneBox <syoutiroukun@gmail.com>'
+  # config.action_mailer.default_from = ENV['MAIL_FROM'] || 'TuneBox <syoutiroukun@gmail.com>'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
