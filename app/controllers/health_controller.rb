8d5cbbdef00858c05aa408472e9da6baaeb7aac5
syouti <syoutiroukun@gmail.com>
2025-08-29 11:12:16 +0900

    スクリーンショット背景問題の修正 - 透明背景を黒背景に変更

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,6 +1,6 @@
 class HealthController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def check
     render json: { status: 'ok', timestamp: Time.current }, status: :ok
   end
