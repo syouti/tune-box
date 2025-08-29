131c4a17ef240fb14c703a0f25a4573bbe69ab78
syouti <syoutiroukun@gmail.com>
2025-08-30 06:13:48 +0900

    ログイン機能の修正: CSRF保護を有効化し、ログイン処理を簡素化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,19 +7,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      # unless user.confirmed?
-      #   flash.now[:alert] = 'メールアドレスの確認が必要です。確認メールを再送信しました。'
-      #   user.send_confirmation_email
-      #   render :new
-      #   return
-      # end
-
       # セッション固定攻撃対策
       reset_session
       session[:user_id] = user.id
 
-      # ログイン情報を記録
-      user.update(last_login_at: Time.current, login_count: user.login_count + 1)
+      # ログイン情報を記録（一時的に無効化）
+      # user.update(last_login_at: Time.current, login_count: user.login_count + 1)
 
       redirect_to favorite_albums_path, notice: "#{user.name}さん、おかえりなさい！キャンバスページに移動しました。"
     else
